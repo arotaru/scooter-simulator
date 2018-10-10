bf513079a8c4bb079d4509b6a406d546369905fd
user <user@users-MacBook-Pro.local>
2018-10-10 17:29:44 -0600

    adding 'done' to end the block; also removing the two blocks that wait for the config and discovery services to be up, since the gateway service already waits on those

--- charger-network-manager/charger-network-manager-entrypoint.sh
+++ charger-network-manager/charger-network-manager-entrypoint.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
-  
-while ! nc -z charger-network-config 8101 ; do
-    echo "Waiting for upcoming Config Server"
-    sleep 60
-done
-while ! nc -z charger-network-discovery 8102 ; do
-    echo "Waiting for upcoming Discovery Server"
-    sleep 60
-done
+
 while ! nc -z charger-network-gateway 8100 ; do
     echo "Waiting for upcoming API Gateway Server"
     sleep 30
+done
 
 java -jar /opt/lib/charger-network-manager-0.0.1-SNAPSHOT.jar
 
