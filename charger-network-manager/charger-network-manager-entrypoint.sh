1fddcc11f5dca616670b4810373399b1e3e216af
user <user@users-MacBook-Pro.local>
2018-10-14 20:23:56 -0600

    increasing sleep from 30->60; this is the wait for the dependency API Gateway sever to come up

--- charger-network-manager/charger-network-manager-entrypoint.sh
+++ charger-network-manager/charger-network-manager-entrypoint.sh
@@ -2,7 +2,7 @@
 
 while ! nc -z charger-network-gateway 8100 ; do
     echo "Waiting for upcoming API Gateway Server"
-    sleep 30
+    sleep 60
 done
 
 java -jar /opt/lib/charger-network-manager-0.0.1-SNAPSHOT.jar
