59073ae05256540f54b1d8f92323e742324457eb
user <user@users-MacBook-Pro.local>
2018-10-24 12:47:39 -0600

    initial test addition

---
+++ charger-client-simulator/src/test/java/com/charger/simulator/controller/test/ControllerTest.java
@@ -0,0 +1,16 @@
+package com.charger.simulator.controller.test;
+
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+//@RunWith(MockitoJUnitRunner.class)
+public class ControllerTest {
+	
+//	private MockMvc mvc;
+//	
+//	@InjectMocks
+//	ChargerClient controller;
+
+}
