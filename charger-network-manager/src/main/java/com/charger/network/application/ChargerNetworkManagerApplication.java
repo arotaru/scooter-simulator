5571376513ab62f5e64864282b746ece42cf175d
user <user@users-MacBook-Pro.local>
2018-10-24 12:34:02 -0600

    trivial: removing empty line

--- charger-network-manager/src/main/java/com/charger/network/application/ChargerNetworkManagerApplication.java
+++ charger-network-manager/src/main/java/com/charger/network/application/ChargerNetworkManagerApplication.java
@@ -14,7 +14,6 @@ public class ChargerNetworkManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChargerNetworkManagerApplication.class, args);
-
 	}
 	
 	@Bean
