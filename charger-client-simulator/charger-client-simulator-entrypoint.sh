09d371d5646325829e971af442cf7c486d052dd0
user <user@users-MacBook-Pro.local>
2018-10-24 12:25:23 -0600

    Shortening wait time for API Gateway from 120->60 sec

--- charger-client-simulator/charger-client-simulator-entrypoint.sh
+++ charger-client-simulator/charger-client-simulator-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 while ! nc -z charger-network-gateway 8100 ; do
-    echo "Waiting another 120 seconds for upcoming API Gateway Server"
-    sleep 120
+    echo "Waiting another 60 seconds for upcoming API Gateway Server"
+    sleep 60
 done
 
 java -jar /opt/lib/charger-client-simulator-0.0.1-SNAPSHOT.jar
