d61cfef4d086375806378c3e09b10b66bb97e32d
user <user@users-MacBook-Pro.local>
2018-10-24 12:48:47 -0600

    initial test addition

---
+++ charger-network-manager/src/test/java/com/charger/network/controller/test/NetworkManagerControllerTest.java
@@ -0,0 +1,35 @@
+package com.charger.network.controller.test;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.charger.network.common.NewAccountResponse;
+import com.charger.network.web.controller.NetworkManagerController;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+//@RunWith(MockitoJUnitRunner.class)
+public class NetworkManagerControllerTest {
+
+//	private MockMvc mvc;
+//	
+//	@InjectMocks
+//	NetworkManagerController controller;
+//	
+//	private JacksonTester<NewAccountResponse> jacksonResponseTester;
+//	
+//	@Before
+//    public void setup() {
+//		JacksonTester.initFields(this, new ObjectMapper());
+//		
+//		mvc = MockMvcBuilders.standaloneSetup(controller)
+//                .build();
+//	}
+	
+	
+	
+}
