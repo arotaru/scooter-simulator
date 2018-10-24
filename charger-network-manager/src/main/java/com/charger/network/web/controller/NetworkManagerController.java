27fdd4634d9fef72c65f62bf262e00ac410ead8a
user <user@users-MacBook-Pro.local>
2018-10-24 12:37:47 -0600

    updating endpoint methods to send/receive String objects at this time; also adding some logging sysout calls for now

--- charger-network-manager/src/main/java/com/charger/network/web/controller/NetworkManagerController.java
+++ charger-network-manager/src/main/java/com/charger/network/web/controller/NetworkManagerController.java
@@ -1,5 +1,6 @@
 package com.charger.network.web.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.apache.http.client.config.RequestConfig;
@@ -8,7 +9,6 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.ClientHttpRequestFactory;
@@ -19,9 +19,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.charger.network.common.NewAccountRequest;
 import com.charger.network.service.NewAccountService;
-import com.scooter.common.ChargerApplicant;
-import com.scooter.common.NewAccountResponse;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 public class NetworkManagerController {
@@ -48,25 +52,55 @@ public class NetworkManagerController {
 	}
 	
 	@RequestMapping(value = "/apply-for-new-account", method = RequestMethod.POST)
-	public NewAccountResponse applyForNewAccount(@RequestBody ChargerApplicant chargerApplicant) {
-		System.out.println("Got the request");
+	public String applyForNewAccount(@RequestBody String jsonObj) {
+		System.out.println("Network Manager Controller - received an /apply-for-new-account request");
 		
 		//TODO: Put charger-accounts-account-manager in a properties file?
 		String newAccountServiceUrl = getServiceUrl("charger-account-manager");
 		
 		//TODO: put /apply-for-new-account in a properties file?
+		
+		//TODO: Notice that here you're sending directly to the service, and not going through the
+		//      API GAteway, like in the ChargerSimulatorApplication...
 		String url = newAccountServiceUrl + "/apply-for-new-account";
+		
+		System.out.println("Sending to: " + newAccountServiceUrl);
 		
 		ClientHttpRequestFactory requestFactory = getClientHttpRequestFactory();
 		restTemplate.setRequestFactory(requestFactory);
 		
 		HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-		 
-		HttpEntity<ChargerApplicant> request = new HttpEntity<>(chargerApplicant, headers);
-		NewAccountResponse response = restTemplate.postForObject(url, request, NewAccountResponse.class);
+        
+        ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		NewAccountRequest newAccountRequest = null;
 		
-		return response;
+		try {
+			newAccountRequest = mapper.readValue(jsonObj, NewAccountRequest.class);
+		} catch (JsonParseException | JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (newAccountRequest == null) {
+				newAccountRequest = new NewAccountRequest();
+			}
+		}
+		
+		System.out.println("Sending directly to " + url + " bypassing API Gateway; should fix this...");
+		
+		String request = "";
+		
+		try {
+			request = mapper.writeValueAsString(newAccountRequest);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		String jsonResponseObj = restTemplate.postForObject(url, request, String.class);
+		
+		System.out.println("Charger-Network-Manager received: " + jsonResponseObj);
+		
+		return jsonResponseObj;
 	}
 	
 	private ClientHttpRequestFactory getClientHttpRequestFactory() {
