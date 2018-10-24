872490eb7490bc08aa5ab801c0b801ae5af977fb
Erik Rotaru <ErikRotaru@gmail.com>
2018-10-24 17:49:02 -0600

    removed boiler plate getters/setters... and replaced with Lombox @Data annotation

--- charger-network-manager/src/main/java/com/charger/network/common/AccountResponse.java
+++ charger-network-manager/src/main/java/com/charger/network/common/AccountResponse.java
@@ -1,23 +1,10 @@
 package com.charger.network.common;
 
-public class AccountResponse {
+import lombok.Data;
+
+public @Data class AccountResponse {
 
 	private String id;
 	private String dateCreated;
-	
-	public AccountResponse() {}
-	
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getDateCreated() {
-		return dateCreated;
-	}
-	public void setDateCreated(String dateCreated) {
-		this.dateCreated = dateCreated;
-	}
 	
 }
