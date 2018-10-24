b1a1c03678e99f9442fa840427f3ca566047488a
user <user@users-MacBook-Pro.local>
2018-10-24 12:14:25 -0600

    Including spring.data.mongodb.uri command line option

--- charger-account-manager/charger-account-manager-entrypoint.sh
+++ charger-account-manager/charger-account-manager-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 while ! nc -z charger-network-gateway 8100 ; do
-    echo "Waiting for upcoming API Gateway Server"
+    echo "Waiting another 60 seconds for upcoming API Gateway Server"
     sleep 60
 done
 
-java -jar /opt/lib/charger-account-manager-0.0.1-SNAPSHOT.jar
+java -Dspring.data.mongodb.uri=$MONGODB_URI -jar /opt/lib/charger-account-manager-0.0.1-SNAPSHOT.jar
